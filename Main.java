c442b77199eb3c53d22f8be7ba44ef75908916d1
Taynara <taynaradasilva2001@gmail.com>
2025-03-18 11:04:42 -0300

    modificação da classe main

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
-import aula01.Gerente;
+import aula01.edificacoes.*;;
 
 public class Main {
     final static Scanner LER = new Scanner(System.in);
     public static void main(String[] args) {
-        Gerente gerente01 = new Gerente("Fernada", "000.111.222-44", 50000.00, 1234, 2000);
-        gerente01.autenticar(1234);
+        Predio predio01 = new Predio(300000.00, 200000.00, 15, 3, 500, 1000);
+        Casa casa01 = new Casa(10000, 70000, 1, 8, 3, true);
+
+        System.out.println("Quantidade de apartamentos:" + predio01.getQtdApartamentos());
+        System.out.println("Está mobilhada? " + (casa01.isEstaMobilhada() == true ? "Sim" : "Não"));
     }
 }
 
