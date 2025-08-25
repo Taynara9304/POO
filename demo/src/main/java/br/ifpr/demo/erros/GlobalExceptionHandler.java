a9c350f79e8a257ec78e2f3e5e3dc0efb3d8b852
Taynara <taynaradasilva2001@gmail.com>
2025-08-25 08:19:31 -0300

    oi

---
+++ demo/src/main/java/br/ifpr/demo/erros/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.ifpr.demo.erros;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice //avisa que a classe é um "tratador" de erros
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERRO:" + e.getMessage());
+    } 
+
+    //outra forma de tratar erros
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle02(Exception e){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "https://http.cat/404.jpg");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+    } 
+
+}
